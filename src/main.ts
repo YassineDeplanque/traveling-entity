a3f2dd04248910a064b46b998d575b1200890a24
Yassine Deplanque <yassine59958@gmail.com>
2026-06-28 12:23:14 +0200

    first body

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 if (!ctx) throw new Error("CTX is NULL");
 
+const body = canvas.getContext("2d");
+if(!body) throw new Error("Body is NULL");
+
 let targetX = canvas.width / 2;
 let targetY = canvas.height / 2;
 
@@ -25,15 +28,22 @@ window.addEventListener("mousemove", (e: MouseEvent) => {
     duration: 10,
     ease: "power2.out",
   });
-});
+})
 
 function animate() {
   if (!ctx) throw new Error("CTX is NULL");
+  if(!body) throw new Error("Body is NULL");
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.beginPath();
   ctx.arc(beast.x, beast.y, 20, 0, Math.PI * 2);
   ctx.stroke();
+
+  body.beginPath();
+  body.moveTo(beast.x, beast.y);
+  body.lineTo(beast.x -200, beast.y -200);
+  body.stroke();
 
   requestAnimationFrame(animate);
 }
